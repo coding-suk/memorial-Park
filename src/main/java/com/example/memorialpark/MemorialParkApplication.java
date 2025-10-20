18222e9258b98ecb6892979505f0d5b15f2ba1f3
coding-suk <hjldd0523@naver.com>
2025-10-20 22:43:40 +0900

    first init

---
+++ src/main/java/com/example/memorialpark/MemorialParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.memorialpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorialParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorialParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memorialpark/MemorialParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memorialpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorialParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
